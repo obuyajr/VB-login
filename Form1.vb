5f3199670592c07fe083da2a270dd6f0431625d5
obuyajr <felix37brock@gmail.com>
2023-05-31 13:15:44 +0300

    initial

--- Form1.vb
+++ Form1.vb
@@ -1,36 +1,39 @@
 ﻿
-'db connection
 Imports System.Data.SqlClient
-
 
 Public Class Form1
     Private Sub btn_login_Click(sender As Object, e As EventArgs) Handles btn_login.Click
-        'www
-        Dim con As SqlConnection = New SqlConnection("Data Source=DESKTOP-KCVKSCU\DESKTOPKCVKSCU;Initial Catalog=vb_login;Integrated Security=True")
-        Dim cmd As SqlCommand = New SqlCommand("select * from user_login where username = ' " & txt_uname.Text & " ' and password ='" & txt_pwd.Text & "' and usertype = '" & combo_usertype.SelectedItem & "' ", con)
+        Dim connectionString As String = "Data Source=DESKTOP-KCVKSCU\DESKTOPKCVKSCU;Initial Catalog=vb_login;Integrated Security=True"
 
-        Dim sda As SqlDataAdapter = New SqlDataAdapter(cmd)
-        Dim dt As DataTable = New DataTable()
+        Using con As SqlConnection = New SqlConnection(connectionString)
+            con.Open()
 
-        sda.Fill(dt)
-        If (dt.Rows.Count > 0) Then
-            MessageBox.Show("you are logged in as" + dt.Rows(0)(2))
-            If (combo_usertype.SelectedIndex = 0) Then
-                Dim a As New Admin
-                a.Show()
+            Dim query As String = "SELECT * FROM user_login WHERE username = @username AND password = @password AND usertype = @usertype"
+            Dim cmd As SqlCommand = New SqlCommand(query, con)
+            cmd.Parameters.AddWithValue("@username", txt_uname.Text)
+            cmd.Parameters.AddWithValue("@password", txt_pwd.Text)
+            cmd.Parameters.AddWithValue("@usertype", combo_usertype.SelectedItem)
+
+            Dim dt As DataTable = New DataTable()
+            Dim sda As SqlDataAdapter = New SqlDataAdapter(cmd)
+            sda.Fill(dt)
+
+            If dt.Rows.Count > 0 Then
+                MessageBox.Show("You are logged in as " & dt.Rows(0)("usertype").ToString())
+
+                If combo_usertype.SelectedIndex = 0 Then
+                    Dim adminForm As New Admin()
+                    adminForm.Show()
+                Else
+                    Dim userForm As New User()
+                    userForm.Show()
+                End If
+
                 Me.Hide()
             Else
-                Dim u As New User
-                u.Show()
-                Me.Hide()
+                MessageBox.Show("Invalid username, password, or user type.")
             End If
-        Else
-                MessageBox.Show("errorrrrrrrr")
-        End If
-
-
-
+        End Using
     End Sub
-
-
 End Class
+
